5308ef2d3e9effde4922e1763dfe10952a05f397
Hüseyin ABACI <huseyin.abaci@hotmail.com>
2023-07-16 21:06:32 +0300

    Update

--- src/BlogList.js
+++ src/BlogList.js
@@ -1,11 +1,13 @@
+import { Link } from "react-router-dom";
+
 const BlogList = ({ blogs }) => {
   return (
     <div className="blog-list">
       {blogs.map(blog => (
-        <div className="blog-preview" key={blog.id} >
+        <Link to={`/blogs/${blog.id}`} className="blog-preview" key={blog.id} >
           <h2>{ blog.title }</h2>
           <p>Written by { blog.author }</p>
-        </div>
+        </Link>
       ))}
     </div>
   );
