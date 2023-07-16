5a8cb68eaada980827cb83e15cb8b73f52021c30
Hüseyin ABACI <huseyin.abaci@hotmail.com>
2023-07-16 21:21:57 +0300

    Final

--- src/Create.js
+++ src/Create.js
@@ -1,15 +1,24 @@
 import { useState } from "react"
+import { useHistory } from "react-router-dom";
 
 const Create = () => {
     const [title,setTitle] = useState("")
     const [body,setBody] = useState("")
     const [author,setAuthor] = useState("Mario")
+    const history = useHistory();
 
     const handleSubmit = (e) => {
         e.preventDefault();
         const blog = {title,body,author}
 
-        console.log(blog);
+        fetch("http://localhost:8000/blogs/",{
+            method:"POST",
+            headers: { "Content-Type": "application/json" },
+            body:JSON.stringify(blog)
+        }).then(() => {
+            console.log("new blog added")
+            history.push("/")
+        })
     }
 
 
